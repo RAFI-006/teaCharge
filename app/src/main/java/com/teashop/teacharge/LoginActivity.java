71d55a8dde9b36828be32d533eb7dbeade5ff22e
pawani <durgapawani123456@outlook.com>
2019-03-21 18:58:32 +0530

    update SplashScree image,update LoginScreen image,UI-Itemlist

--- app/src/main/java/com/teashop/teacharge/LoginActivity.java
+++ app/src/main/java/com/teashop/teacharge/LoginActivity.java
@@ -1,15 +1,26 @@
 package com.teashop.teacharge;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
-
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnClick;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+          btnClick = (Button) findViewById(R.id.btn_login) ;
+        btnClick.setOnClickListener(this);
     }
 
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(LoginActivity.this,ItemListActivity.class);
+        startActivity(intent);
+        finish();
+    }
 }
